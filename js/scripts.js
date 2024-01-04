cc57b0195246df28ea69e3a7edbafcfabcc09cf7
scottie-eves <148817937+scottie-eves@users.noreply.github.com>
2024-01-04 12:22:46 -0800

    App Update Loop and Document.write
    Added simple styling to DOM.
    Added h3 heading and centered text.
    Added loop for the pokemonList to display the pokemon information on the webpage. Added a conditional to any pokemon larger than 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 let pokemonList = [
-    {name: 'Squirtle' , type: ['water', 'speed'] , height: 3} , 
-    {name: 'Charmander' , type: ['fire' , 'strength'] , height: 4} ,
-    {name: 'Geodude' , type: ['rock', 'ground'] , height: 2}
+    {name: 'Squirtle: ' , type: ['water', ' speed'] , height: 3} , 
+    {name: ' Charmander: ' , type: ['fire' , ' strength'] , height: 4} ,
+    {name: ' Geodude: ' , type: ['rock', ' ground'] , height: 2}
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + pokemonList[i].type + ". " + "height: " + pokemonList[i].height);
+    if (pokemonList[i].height > 3){
+    document.write(", that's a big pokemon!");
+}
+}
 
 console.log(pokemonList);
