be65a9ec4d3840fbe1fd889a2895c1a2dffae2c5
scottie-eves <148817937+scottie-eves@users.noreply.github.com>
2024-01-15 12:42:35 -0800

    DOM update
    CSS: added styling to buttons and took away bullets from list.
    
    Index: added new H2 heading. and added class to <ul>
    
    JS: created new pokemonRepository to contain all of the pokemon in one function.
    Created new If else statement to the repository.
    Created new function to select html elements and created lists and buttons from there.
    Created event listener inside function to log the pokemon's name in the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,68 @@
-let pokemonList = [
-    {name: 'Squirtle: ' , type: ['water', ' speed'] , height: 3} , 
-    {name: ' Charmander: ' , type: ['fire' , ' strength'] , height: 4} ,
-    {name: ' Geodude: ' , type: ['rock', ' ground'] , height: 2}
+let pokemonRepository = (function () {
+    let repository = [
+    {name: ' Squirtle ' , type: ['water', ' speed'] , height: 3} , 
+    {name: ' Charmander ' , type: ['fire' , ' strength'] , height: 4} ,
+    {name: ' Geodude ' , type: ['rock', ' ground'] , height: 2} ,
+    {name: ' Pikachu ' , type: ['electric', ' flying'] , height: 1} , 
+    {name: ' Cubone ' , type: ['rock' , ' ground'] , height: 0.5} ,
+    {name: ' Diglett ' , type: ['rock', ' ground'] , height: 0.2}
 ];
 
-for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + pokemonList[i].type + ". " + "height: " + pokemonList[i].height);
-    if (pokemonList[i].height > 3){
-    document.write(", that's a big pokemon!");
+function add(pokemon) {
+    if (
+        typeof pokemon === 'object' &&
+        'name' in pokemon &&
+        'height' in pokemon &&
+        'type' in pokemon
+    ) {
+        repository.push(pokemon);        
+    } else {
+        console.log('That is incorrect');
+    }
 }
+function getAll() {
+    return repository;
 }
+function showDetails(pokemon){
+    console.log(pokemon.name);
+}
+function addListItem(pokemon){
+    let ulItem = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    ulItem.appendChild(listItem);
+    button.addEventListener('click', function(showDetails) {
+        console.log(pokemon.name);
+    })
+}
+return {
+    add: add ,
+    getAll: getAll ,
+    addListItem: addListItem
+};
+})();
 
-console.log(pokemonList);
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
+
+console.log(pokemonRepository.getAll());
+
+// function divide(dividend, divisor) {
+
+//     if (divisor === 0) {
+//         return 'you are trying to divide by zero'
+//     } else {
+//         let result = dividend/divisor;
+//         return result;
+//     }
+// }
+
+// console.log(divide(4, 2));
+// console.log(divide(7, 0));
+// console.log(divide(1, 4));
+// console.log(divide(12, -3));
+// console.log(divide(-4, -2));
